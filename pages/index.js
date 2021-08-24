2eba1f8df056f639727f5c18814f59e863285b2f
Jose Chavez <jose.e.chavez@gmail.com>
2021-08-23 20:25:13 -0500

    Refactored into functions; not using semicolons

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,16 @@ import { DateTime } from 'luxon'
 import _ from 'lodash'
 
 export async function getServerSideProps(context) {
-  const today = DateTime.now();
-  const startDate = today.toFormat('yyyy-LL-dd');
-  const endDate = today.plus({ days: 1 }).toFormat('yyyy-LL-dd');
-  const apiKey = 'X'; // TODO fill in with the right api key
+  const today = DateTime.now()
+  const startDate = today.toFormat('yyyy-LL-dd')
+  const endDate = today.plus({ days: 1 }).toFormat('yyyy-LL-dd')
+  const apiKey = 'X' // TODO fill in with the right api key
   
-  const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
+  const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`
   
   // Fetch data from external API
-  const res = await fetch(url);
-  const data = await res.json();
+  const res = await fetch(url)
+  const data = await res.json()
 
   // Pass data to the page via props
   return { props: { data } }
@@ -22,40 +22,13 @@ export async function getServerSideProps(context) {
 
 export default function Home({ data }) {
   // Transform data
-  const payload = data.near_earth_objects;
-  const rows = [];
-  _.values(payload).forEach(value => {
-    value.forEach(neo => {
-      const row = {
-        name: _.get(neo, 'name', '-'),
-        jplUrl: _.get(neo, 'nasa_jpl_url', '#'),
-        isHazardous: _.get(neo, 'is_potentially_hazardous_asteroid', '-'),
-        approachDate: _.get(neo, 'close_approach_data[0].close_approach_date_full', '-'),
-        missDistance: _.get(neo, 'close_approach_data[0].miss_distance.kilometers', '-'),
-        relativeVelocity: _.get(neo, 'close_approach_data[0].relative_velocity.kilometers_per_hour', '-'),
-        diameterMin: _.get(neo, 'estimated_diameter.kilometers.estimated_diameter_min', '-'),
-        diameterMax: _.get(neo, 'estimated_diameter.kilometers.estimated_diameter_max', '-')
-      };
-      rows.push(row);
-    });
-  });
+  const rows = parseData(data)
 
-  const sortedRows = _.sortBy(rows, ['approachDate']);
+  const sortedRows = _.sortBy(rows, ['approachDate'])
 
   // Create table rows
-  const tableRows = [];
-  sortedRows.forEach(row => {
-    tableRows.push(
-      <tr>
-        <td className={styles.tableCell}><a href={row.jplUrl}>{row.name}</a></td>
-        <td className={styles.tableCell}>{row.isHazardous ? 'Y' : 'N'}</td>
-        <td className={styles.tableCell}>{row.approachDate}</td>
-        <td className={styles.tableCell}>{row.missDistance}</td>
-        <td className={styles.tableCell}>{row.relativeVelocity}</td>
-        <td className={styles.tableCell}>{row.diameterMin}/{row.diameterMax}</td>
-      </tr>
-    );
-  });
+  const tableRows = createTableRows(sortedRows)
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -81,4 +54,43 @@ export default function Home({ data }) {
       </main>
     </div>
   )
+}
+
+function parseData(data) {
+  const payload = data.near_earth_objects
+  const rows = []
+  _.values(payload).forEach(value => {
+    value.forEach(neo => {
+      const row = {
+        name: _.get(neo, 'name', '-'),
+        jplUrl: _.get(neo, 'nasa_jpl_url', '#'),
+        isHazardous: _.get(neo, 'is_potentially_hazardous_asteroid', '-'),
+        approachDate: _.get(neo, 'close_approach_data[0].close_approach_date_full', '-'),
+        missDistance: _.get(neo, 'close_approach_data[0].miss_distance.kilometers', '-'),
+        relativeVelocity: _.get(neo, 'close_approach_data[0].relative_velocity.kilometers_per_hour', '-'),
+        diameterMin: _.get(neo, 'estimated_diameter.kilometers.estimated_diameter_min', '-'),
+        diameterMax: _.get(neo, 'estimated_diameter.kilometers.estimated_diameter_max', '-')
+      }
+      rows.push(row)
+    })
+  })
+  return rows
+}
+
+function createTableRows(sortedRows) {
+  const tableRows = []
+  sortedRows.forEach(row => {
+    tableRows.push(
+      <tr>
+        <td className={styles.tableCell}><a href={row.jplUrl}>{row.name}</a></td>
+        <td className={styles.tableCell}>{row.isHazardous ? 'Y' : 'N'}</td>
+        <td className={styles.tableCell}>{row.approachDate}</td>
+        <td className={styles.tableCell}>{row.missDistance}</td>
+        <td className={styles.tableCell}>{row.relativeVelocity}</td>
+        <td className={styles.tableCell}>{row.diameterMin}/{row.diameterMax}</td>
+      </tr>
+    )
+  })
+
+  return tableRows
 }
