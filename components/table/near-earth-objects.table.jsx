bc6c7dc69a4363ea04baf7a95f39c129df90accb
Jose Chavez <jose.e.chavez@gmail.com>
2021-09-04 22:31:27 -0500

    Renamed table file, exported data handling to external function

--- components/table/near-earth-objects.table.jsx
+++ components/table/near-earth-objects.table.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { useTable } from 'react-table'
-
-import { makeStyles } from '@material-ui/core/styles';
-
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-
-import _ from 'lodash'
+import React from 'react';
+import { useTable } from 'react-table';
+import parseNeoData from './near-earth-objects';
 
 const useStyles = makeStyles({
   container: {
@@ -25,7 +22,8 @@ const useStyles = makeStyles({
 
 export default function NearEarthObjectsTable({ neodata }) {
   // Transform data
-  const tableData = parseData(neodata)
+  const parsedData = parseNeoData(neodata)
+  const tableData = React.useMemo(() => parsedData)
 
   // Config table
   const columns = columnConfig()
@@ -72,29 +70,6 @@ export default function NearEarthObjectsTable({ neodata }) {
       </Table>
     </TableContainer>
   )
-}
-
-function parseData(data) {
-  const payload = data.near_earth_objects
-  const rows = []
-  _.values(payload).forEach(value => {
-    value.forEach(neo => {
-      const row = {
-        name: _.get(neo, 'name', '-'),
-        jplUrl: _.get(neo, 'nasa_jpl_url', '#'),
-        isHazardous: _.get(neo, 'is_potentially_hazardous_asteroid', '-'),
-        approachDate: _.get(neo, 'close_approach_data[0].close_approach_date_full', '-'),
-        missDistance: _.get(neo, 'close_approach_data[0].miss_distance.kilometers', '-'),
-        relativeVelocity: _.get(neo, 'close_approach_data[0].relative_velocity.kilometers_per_hour', '-'),
-        diameterMin: _.get(neo, 'estimated_diameter.kilometers.estimated_diameter_min', '-'),
-        diameterMax: _.get(neo, 'estimated_diameter.kilometers.estimated_diameter_max', '-')
-      }
-      rows.push(row)
-    })
-  })
-  const sortedRows = _.sortBy(rows, ['approachDate'])
-  const memoizedRows = React.useMemo(() => sortedRows)
-  return memoizedRows
 }
 
 function nameColumnAccessor(row) {
